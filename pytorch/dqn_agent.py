e573985c5e566142c0104c91356757a02fbbefd4
SwaksharDeb <49334830+SwaksharDeb@users.noreply.github.com>
2020-01-15 23:56:41 +0600

    Add files via upload

--- pytorch/dqn_agent.py
+++ pytorch/dqn_agent.py
@@ -90,7 +90,7 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
             
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
         #Get expected Q values from local model
