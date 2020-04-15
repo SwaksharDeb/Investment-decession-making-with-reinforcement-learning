01e6c5d68a6fd8b6e67fae8f5089cc92fe87e024
SwaksharDeb <49334830+SwaksharDeb@users.noreply.github.com>
2020-04-15 21:47:17 +0600

    Add files via upload

--- DDQN_PER.py
+++ DDQN_PER.py
@@ -10,19 +10,21 @@ import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)     # replay buffer size
 BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.995            # discount factor
-TAU = 1e-2              # for soft update of target parameters
-LR = 4.8e-4              # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
+GAMMA = 0.95            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 0.001              # learning rate
+UPDATE_EVERY = 10        # how often to update the network
+UPDATE_EVERY_LR = 250   # how often to update the learning rate scheduler
+UPDATE_EVERY_TARGET = 10  # how often update the target network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed, lr_decay=0.9999):
+    def __init__(self, state_size, action_size, seed, lr_decay=0.999):
         """Initialize an Agent object.
-        
+
         Params
         ======
             state_size (int): dimension of each state
@@ -37,21 +39,28 @@ class Agent():
         # Q-Network
         self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
         self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)   #only update the local network parameters
-        #self.lr_scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, lr_decay)
-         
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)   #only update the                                                 local network parameters
+        self.lr_scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, lr_decay)
+
         # prioritized Replay memory
         self.memory = PrioritizedReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed, device,
                                                   alpha=0.6, beta=0.4, beta_scheduler=1.0)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
-    
+        # Initialize time step learning rate scheduler(for updating every UPDATE_EVERY_LR steps)
+        self.t_step_lr = 0
+        # Initialize time step (for updating every UPDATE_EVERY_TARGET steps)
+
+        self.t_step_target = 0
+
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
-        
+
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        # Learn every UPDATE_EVERY_LR time steps.
+        self.t_step_lr = (self.t_step_lr + 1) % UPDATE_EVERY_LR
         if self.t_step == 0:
         # If enough samples are available in memory, get random subset and learn
             if len(self.memory) > BATCH_SIZE:
@@ -60,7 +69,7 @@ class Agent():
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy and Q value.
-        
+
         Params
         ======
             state (array_like): current state
@@ -81,19 +90,19 @@ class Agent():
             random_action = random.choice(np.arange(self.action_size))
             action_values = action_values.cpu().data.numpy()
             return random_action, action_values[0,0,random_action]
-        
+
     def learn(self, experiences, gamma):
         """Update value parameters using given batch of experience tuples.
 
         Params
         ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done, w) tuples  
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done, w) tuples
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones, w = experiences
-            
+
         # Get max predicted Q values (for next states) from target model
-        
+
         Q_locals_next = self.qnetwork_local(next_states)
         greedy_actions = Q_locals_next.max(dim=1, keepdim=True)[1] # greedy action suggested by local network
         Q_targets_next = self.qnetwork_target(next_states).gather(1,greedy_actions) # Q values of those action w.r.t target network
@@ -108,17 +117,20 @@ class Agent():
         with torch.no_grad():
             TD_error = Q_targets.detach()
             TD_error.pow_(0.5)
-            self.memory.update_priorities(TD_error)            
+            self.memory.update_priorities(TD_error)
         Q_targets.mul_(w)
-        loss = Q_targets.mean()        
+        loss = Q_targets.mean()
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-        #self.lr_scheduler.step()
-    
+        self.t_step_target = (self.t_step_target + 1) % UPDATE_EVERY_TARGET
+        if self.t_step_lr == 0:
+            self.lr_scheduler.step()
+
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)    #updating the target network parameters                    
+        if self.t_step_target == 0:  # update target network after every UPDATE_EVERY_TARGET
+            self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)    #updating the target network parameters
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
@@ -128,7 +140,7 @@ class Agent():
         ======
             local_model (PyTorch model): weights will be copied from
             target_model (PyTorch model): weights will be copied to
-            tau (float): interpolation parameter 
+            tau (float): interpolation parameter
         """
         #self.qnetwork_local.eval()
         #self.qnetwork_target.eval()
@@ -137,7 +149,7 @@ class Agent():
 
 class PrioritizedReplayBuffer:
     """Fixed-size prioritized buffer to store experience tuples."""
-    
+
     def __init__(self, action_size, buffer_size, batch_size, seed, device, alpha=0., beta=1., beta_scheduler=1.):
         """Initialize a PrioritizedReplayBuffer object.
         Params
@@ -150,7 +162,7 @@ class PrioritizedReplayBuffer:
             beta (float): amount of importance-sampling correction; β = 1 fully compensates for the non-uniform probabilities
             beta_scheduler (float): multiplicative factor (per sample) for increasing beta (should be >= 1.0)
         """
-        
+
         self.action_size = action_size
         self.buffer_size = buffer_size
         self.batch_size = batch_size
@@ -159,7 +171,7 @@ class PrioritizedReplayBuffer:
         self.alpha = alpha
         self.beta = beta
         self.beta_scheduler = beta_scheduler
-        
+
         # Create a Numpy Array to store namedtuples of experience
         self.memory = np.empty(buffer_size, dtype=[
             ("state", np.ndarray),
@@ -170,7 +182,7 @@ class PrioritizedReplayBuffer:
             ('prob', np.double)]) # sel.memory = [(s,a,r,s',d,w),(s,a,r,s',d,w)]
         # Variable to control the memory buffer as being a circular list
         self.memory_idx_ctrl = 0
-        
+
         # Variable to control the selected samples
         self.memory_samples_idx = np.empty(batch_size)
         # Numpy Array to store selected samples
@@ -182,18 +194,18 @@ class PrioritizedReplayBuffer:
         # Each new experience is added to the memory with
         # the maximum probability of being choosen
         self.max_prob = 0.0001
-        
+
         # Value to a non-zero probability
         self.nonzero_probability = 0.00001
-        
+
         # Numpy Arrays to store probabilities and weights
         # (tradeoff between memory space and cumputacional processing)
         self.p = np.empty(buffer_size, dtype=np.double)
         self.w = np.empty(buffer_size, dtype=np.double)
-    
+
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
-        
+
         # Add the experienced parameters to the memory
         self.memory[self.memory_idx_ctrl]['state'] = state
         self.memory[self.memory_idx_ctrl]['action'] = action
@@ -201,10 +213,10 @@ class PrioritizedReplayBuffer:
         self.memory[self.memory_idx_ctrl]['next_state'] = next_state
         self.memory[self.memory_idx_ctrl]['done'] = done
         self.memory[self.memory_idx_ctrl]['prob'] = self.max_prob
-        
+
         # Control memory as a circular list
         self.memory_idx_ctrl = (self.memory_idx_ctrl + 1) % self.buffer_size
-    
+
     def sample(self):
         """Sample a batch of prioritized experiences from memory."""
         # Normalize the probability of being chosen for each one of the memory registers
@@ -213,15 +225,15 @@ class PrioritizedReplayBuffer:
         self.memory_samples_idx = np.random.choice(self.buffer_size, self.batch_size, replace=False, p=self.p)
         # Get the samples from memory
         self.memory_samples = self.memory[self.memory_samples_idx]
-        
+
         # Compute importance-sampling weights for each one of the memory registers
         # w = ((N * P) ^ -β) / max(w)
         np.multiply(self.memory['prob'], self.buffer_size, out=self.w)
         np.power(self.w, -self.beta, out=self.w, where=self.w!=0) # condition to avoid division by zero
         np.divide(self.w, self.w.max(), out=self.w) # normalize the weights
-        
+
         self.beta = min(1, self.beta*self.beta_scheduler)
-        
+
         # Split data into new variables
         states = torch.from_numpy(np.vstack(self.memory_samples['state'])).float().to(self.device)
         actions = torch.from_numpy(np.vstack(self.memory_samples['action'])).long().to(self.device)
@@ -229,27 +241,27 @@ class PrioritizedReplayBuffer:
         next_states = torch.from_numpy(np.vstack(self.memory_samples['next_state'])).float().to(self.device)
         dones = torch.from_numpy(np.vstack(self.memory_samples['done'])).float().to(self.device)
         weights = torch.from_numpy(self.w[self.memory_samples_idx]).float().to(self.device)
-        
+
         return (states, actions, rewards, next_states, dones, weights)
-    
+
     def update_priorities(self, td_error):
         # Balance the prioritization using the alpha value
         td_error.pow_(self.alpha)
 
         # Guarantee a non-zero probability
         td_error.add_(self.nonzero_probability)
-        
+
         #convert cuda tensor to numpy array
-        td_error = td_error.cpu().data.numpy()        
-        
+        td_error = td_error.cpu().data.numpy()
+
         # Update the probabilities in memory
         self.memory_samples['prob'] = td_error
         self.memory[self.memory_samples_idx] = self.memory_samples
-        
+
         # Update the maximum probability value
         self.max_prob = self.memory['prob'].max()
-        
-       
+
+
     def __len__(self):
         """Return the current size of internal memory."""
         return self.buffer_size if self.memory_idx_ctrl // self.buffer_size > 0 else self.memory_idx_ctrl
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=256, fc3_units=128,fc4_units=128):
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=256, fc3_units=256,fc4_units=128,fc5_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -24,17 +24,18 @@ class QNetwork(nn.Module):
         self.fc3 = nn.Linear(fc2_units, fc3_units)
         #self.dropout_3 = nn.Dropout(p=0.2)
         self.fc4 = nn.Linear(fc3_units, fc4_units)
-        self.fc5 = nn.Linear(fc4_units,action_size)
-
+        self.fc5 = nn.Linear(fc4_units,fc5_units)
+        self.fc6 = nn.Linear(fc5_units,action_size)
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
+        x = F.leaky_relu(self.fc1(state))
         #x = self.dropout_1(x)
-        x = F.relu(self.fc2(x))
+        x = F.leaky_relu(self.fc2(x))
         #x = self.dropout_2(x)
-        x = F.relu(self.fc3(x))
+        x = F.leaky_relu(self.fc3(x))
         #x = self.dropout_3(x)
-        x = F.relu(self.fc4(x))
-        return self.fc5(x)
+        x = F.leaky_relu(self.fc4(x))
+        x = F.leaky_relu(self.fc5(x))
+        return self.fc6(x)
 
 
